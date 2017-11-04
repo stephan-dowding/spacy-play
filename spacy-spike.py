f14666fac7f6929e0324f45e98b07249173fd42a
Stephan Dowding <stephan.dowding@gmail.com>
2017-11-04 22:19:20 +0800

    removed useless words, lemmatised

--- spacy-spike.py
+++ spacy-spike.py
@@ -1,4 +1,11 @@
 import spacy
-nlp = spacy.load('en')
+
+def custom_pipeline(nlp):
+    return (nlp.tagger, nlp.parser, nlp.entity)
+
+nlp = spacy.load('en', create_pipeline=custom_pipeline)
+
 doc = nlp(u'hello this is some text I hope you like reading it')
 print([(w.text, w.pos_) for w in doc])
+print([w.lemma_ for w in doc])
+print([w.lemma_ for w in doc if not w.is_stop])
